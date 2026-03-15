85b37127434d4d1bbd2096a8c60249936caa851e
KaoriA <138273671+lilythelily@users.noreply.github.com>
2026-03-15 13:40:59 +0900

    updated

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const desktopList = document.querySelectorAll(".desktop-ul li");
 const prevBtns = document.querySelectorAll(".previous-btn");
 const nextBtns = document.querySelectorAll(".next-btn");
 const hero = document.querySelector(".section__hero");
-const slides = document.querySelectorAll(".hero-container");
+let slides = document.querySelectorAll(".hero-container");
 
 // === navigation hover ===
 
@@ -19,64 +19,109 @@ desktopList.forEach((menu) => {
 
 // === gallery display control ===
 
-let currentSlide = 0;
-const totalSlides = slides.length;
+let currentSlide = 1;
+let totalSlides = slides.length;
+transform();
+addTransition();
 
-function updateSlidePosition() {
-  hero.style.transform = `translateX(-${currentSlide * 100}vw)`;
+function transform() {
+  hero.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function navigate(index, withTransition = true) {
+  if (withTransition) addTransition();
+  else removeTransition();
+  currentSlide = index;
+  transform();
+}
+
+function nextSlide() {
+  navigate(currentSlide + 1, true);
+}
+
+function previousSlide() {
+  navigate(currentSlide - 1, true);
+}
+
+function removeTransition() {
+  hero.classList.remove("slideTransition");
 }
 
 function addTransition() {
   hero.classList.add("slideTransition");
 }
-function removeTransition() {
-  hero.classList.remove("slideTransition");
-}
-
-// === transitionend?? ===
 
 nextBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    currentSlide = (currentSlide + 1) % totalSlides;
-    updateSlidePosition();
-    if (btn.classList.contains("nextBtn4")) {
-      removeTransition();
-    } else {
-      addTransition();
-    }
-  });
+  btn.addEventListener("click", nextSlide);
 });
 
 prevBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-    if (btn.classList.contains("prevBtn1")) {
-      removeTransition();
-    } else {
-      addTransition();
-    }
-    updateSlidePosition();
-  });
+  btn.addEventListener("click", previousSlide);
 });
 
 // === keyboard control ===
 
 document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowRight") {
-    currentSlide = (currentSlide + 1) % totalSlides;
-    updateSlidePosition();
-    if (currentSlide == totalSlides - 1) {
-      removeTransition();
-    } else {
-      addTransition();
-    }
+    nextSlide();
   } else if (e.key === "ArrowLeft") {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-    updateSlidePosition();
-    if (currentSlide == totalSlides - 1) {
-      removeTransition();
+    previousSlide();
+  }
+});
+
+// === swipe control ===
+
+let touchStartX = 0;
+let touchEndX = 0;
+let touchStartY = 0;
+let touchEndY = 0;
+
+function swipe() {
+  const diffX = touchEndX - touchStartX;
+  const diffY = touchEndY - touchStartY;
+
+  if (Math.abs(diffX) > Math.abs(diffY)) {
+    if (diffX > 0) {
+      console.log('right');
+      nextSlide();
     } else {
-      addTransition();
+      previousSlide();
     }
   }
+}
+
+hero.addEventListener('touchstart', (e) => {
+  touchStartX = e.changedTouches[0].screenX;
+  touchStartY = e.changedTouches[0].screenY;
+}, false);
+
+hero.addEventListener('touchend', (e) => {
+  touchEndX = e.changedTouches[0].screenX;
+  touchEndY = e.changedTouches[0].screenY;
+  swipe();
+}, false)
+
+// === transition control ===
+
+hero.addEventListener("transitionend", () => {
+  slides = document.querySelectorAll(".hero-container");
+  totalSlides = slides.length;
+
+  if (currentSlide === totalSlides - 1) {
+    removeTransition();
+    currentSlide = 1;
+    transform();
+    setTimeout(addTransition, 20);
+    return;
+  }
+
+  if (currentSlide === 0) {
+    removeTransition();
+    currentSlide = totalSlides - 2;
+    transform();
+    setTimeout(addTransition, 20);
+    return;
+  }
+
+  addTransition();
 });
